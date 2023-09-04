d11e14303d3e31c142b6ae2fe4c95567986e46aa
Mark-Pino <mark.pino@upeu.edu.pe>
2023-09-04 10:02:15 -0500

    ms-cliente

---
+++ ms-cliente/src/main/java/com/example/mscliente/MsClienteApplication.java
@@ -0,0 +1,13 @@
+package com.example.mscliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsClienteApplication.class, args);
+	}
+
+}
