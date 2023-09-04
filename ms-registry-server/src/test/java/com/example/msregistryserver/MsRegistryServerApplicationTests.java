b964a876d3618b34163aede75cd4ea46201330e1
Mark-Pino <mark.pino@upeu.edu.pe>
2023-09-03 20:52:30 -0500

    registry-server

---
+++ ms-registry-server/src/test/java/com/example/msregistryserver/MsRegistryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msregistryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRegistryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
