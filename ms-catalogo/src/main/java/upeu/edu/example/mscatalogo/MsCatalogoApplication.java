f44171c0b683a09a15a54a5354d8da2e82f52f0a
Mark-Pino <mark.pino@upeu.edu.pe>
2023-09-03 20:17:12 -0500

    config-server

--- ms-catalogo/src/main/java/upeu/edu/example/mscatalogo/MsCatalogoApplication.java
+++ ms-catalogo/src/main/java/upeu/edu/example/mscatalogo/MsCatalogoApplication.java
@@ -1,7 +1,11 @@
 package upeu.edu.example.mscatalogo;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MsCatalogoApplication {
@@ -9,5 +13,14 @@ public class MsCatalogoApplication {
     public static void main(String[] args) {
         SpringApplication.run(MsCatalogoApplication.class, args);
     }
-
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new Info()
+                .title("OPEN API MICROSERVICIO CATALOGO")
+                .version("0.0.1")
+                .description("servicios web catálogo")
+                .termsOfService("http:// swagger.io/terms")
+                .license(new License().name("Apache 2.0").url("http://springdoc.org"))
+        );
+    }
 }
