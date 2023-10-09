1ce95739271b4eaaaafeef4146ed808c52d2e421
Mark-Pino <mark.pino@upeu.edu.pe>
2023-10-09 08:16:24 -0500

    ms-auth

---
+++ ms-auth/src/test/java/com/example/msauth/MsAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
