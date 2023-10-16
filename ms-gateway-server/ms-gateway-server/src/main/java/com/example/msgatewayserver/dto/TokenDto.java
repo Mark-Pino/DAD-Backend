141afac71388569974400553e84462d4d8862b37
Mark-Pino <mark.pino@upeu.edu.pe>
2023-10-16 08:50:02 -0500

    asfgaga

---
+++ ms-gateway-server/ms-gateway-server/src/main/java/com/example/msgatewayserver/dto/TokenDto.java
@@ -0,0 +1,14 @@
+package com.example.msgatewayserver.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class TokenDto {
+    private String token;
+}
